13309201b62984bd244bd6564868e7379c27be51
Hamidhrf <hamidrezafathollahzadeh@gmail.com>
2025-06-12 19:58:42 +0200

    Update greeting message

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World From It Works On My Machine!');
+  res.send('Hello World From It Works On My Machine Team!');
 });
 
 module.exports = app;
