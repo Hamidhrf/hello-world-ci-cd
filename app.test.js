d7c20a2c64951afedaf267a851943d035c666dea
Hamidreza Fathollahzadeh <fatollahzadeh.hrf@gmail.com>
2025-06-12 20:38:25 +0000

    Edit app.test.js

--- app.test.js
+++ app.test.js
@@ -4,6 +4,6 @@ const app = require('./app');
 test('GET / responds with Hello World', async () => {
   const res = await request(app).get('/');
   expect(res.statusCode).toBe(200);
-  expect(res.text).toBe('Hello World From It Works On My Machine!');
+  expect(res.text).toBe('Hello World From It Works On My Machine Team!');
 });
 
